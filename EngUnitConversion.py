555e044d210a5150e8e645fa6c525989b2fa1737
Frank Pereny <56854037+fjpereny@users.noreply.github.com>
2019-11-21 23:30:14 +0800

    Update EngUnitConversion.py
    Added Force class.

--- EngUnitConversion.py
+++ EngUnitConversion.py
@@ -1,5 +1,5 @@
-class Unit(object):
-    """Unit object containing a float value and string unit."""
+class EngUnit(object):
+    """Generic class for engineering unit objects containing a float value and string unit."""
     
     numerator = []
     denominator = []
@@ -59,14 +59,25 @@ class Unit(object):
         new_value = self.value // other
         return self.__class__(new_value, self.unit)
 
-class TempUnit(Unit):
-    K = 'K'
-    C = 'C'
-    R = 'R'
-    F = 'F'
+    def __pow__(self, other):
+        new_value = self.value ** other
+        return self.__class__(new_value, self.unit)
 
+
+class Temperature(EngUnit):
+    """Creates a temperature object that can store a temperature value and
+    convert between units of temperature."""
+    
+    class Unit():
+        K = 'K'
+        C = 'C'
+        R = 'R'
+        F = 'F'
+
+    # conversions { } is not used to convert the Temperature() class because 
+    # temperature is not converted with a scalar.  See the convert() function below.
     conversions = {
-        'K' : 1,
+        'K' : 1.0,
     }
 
     def convert(self, to_unit):
@@ -117,7 +128,62 @@ class TempUnit(Unit):
         else:
             return None    
 
-class LengthUnit(Unit):
+    def __add__(self, other):
+        self_original_unit = self.unit
+
+        self.changeUnit('K')
+
+        if other.unit in ('K', 'C'):
+            new_value = self.value + other.value
+        else:
+            new_value = self.value + (other.value * 5.0 / 9.0)            
+
+        new_unit = self.__class__(new_value, 'K')
+        new_unit.changeUnit(self_original_unit)
+        return new_unit
+
+    def __sub__(self, other):
+            self_original_unit = self.unit
+
+            self.changeUnit('K')
+
+            if other.unit in ('K', 'C'):
+                new_value = self.value - other.value
+            else:
+                new_value = self.value - (other.value * 5.0 / 9.0)            
+
+            new_unit = self.__class__(new_value, 'K')
+            new_unit.changeUnit(self_original_unit)
+            return new_unit
+
+
+class Length(EngUnit):
+    """Creates a length object that can store a length value and 
+    convert between units of length."""
+    
+    class Unit():
+        fm = 'fm' 
+        pm = 'pm' 
+        nm = 'nm' 
+        um = 'um' 
+        mm = 'mm' 
+        cm = 'cm'
+        m = 'm'
+        dam = 'dam'
+        hm = 'hm' 
+        km = 'km'
+        Mm = 'Mm' 
+        Gm = 'Gm'
+        Tm = 'Tm'
+        Pm = 'Pm'
+
+        inch = 'inch'
+        ft = 'ft'
+        yd = 'yd'
+        mi = 'mi'
+
+        nautMi = 'nautMi'
+        lightYear = 'lightYear'
 
     conversions = {
         'fm' : 1000000000000000,
@@ -126,7 +192,7 @@ class LengthUnit(Unit):
         'um' : 1000000,
         'mm' : 1000,
         'cm' : 100,
-        'm' : 1,
+        'm' : 1.0,
         'dam' : 0.1,
         'hm' : 0.01,
         'km' : 0.001,
@@ -135,56 +201,74 @@ class LengthUnit(Unit):
         'Tm' : 0.000000000001,
         'Pm' : 0.000000000000001,
 
-        'in' : 39.3701,
+        'inch' : 39.3701,
         'ft' : 3.28084,
         'yd' : 1.09361,
-        'mi' : 0.000621371
+        'mi' : 0.000621371,
+
+        'nautMi' : 1.0 / 1852.0,
+        'lightYear' : 1.0 / (9.4607304725808 * (10**15))
     }
 
-class CurrentUnit(Unit):
-    amp = 'A'
-    mAmp = 'mA'
-    kAmp = 'kA'
+
+class Current(EngUnit):
+    """Creates a current (amperage) object that can store a current (amperage) value and 
+    convert between units of current (amperage)."""
+    
+    class Unit():
+        A = 'A'
+        mA = 'mA'
+        kA = 'kA'
+    
     conversions = {
         'A' : 1,
         'mA' : 1000,
         'kA' : 0.001
     }
 
-class TimeUnit(Unit):
-    ms = 'ms'
-    sec = 's'
-    minute = 'min'
-    hr = 'hr'
-    day = 'day'
+
+class EngTime(EngUnit):
+    """Creates a time object that can store a time value and 
+    convert between units of time."""
+    
+    class Unit():
+        ms = 'ms'
+        s = 's'
+        minute = 'minute'
+        hr = 'hr'
+        day = 'day'
     
     conversions = {
-        'ms' : 1000,
-        's' : 1,
-        'min' : 1.0 / 60.0,
+        'ms' : 1000.0,
+        's' : 1.0,
+        'minute' : 1.0 / 60.0,
         'hr' : 1.0 / 60.0 / 60.0,
         'day' : 1.0 / 60.0 / 60.0 / 24.0
     }
 
-class PressureUnit(Unit):
-    bar = 'bar'
-    mbar = 'mbar'
-    ubar = 'ubar'
-    atm = 'atm'
-    Pa = 'Pa'
-    hPa = 'hPa'
-    kPa = 'kPa'
-    MPa = 'MPa'
-    mmHg = 'mmHg'
-    Torr = 'mmHg'
-    mmH2O = 'mmH2O'
-    mH2O = 'mH2O'
-    psi = 'psi'
-    ftH2O = 'ftH2O'
-    inH2O = 'inH2O'
-    inHg = 'inHg'
-    kgcm2 = 'kgcm2'
 
+class Pressure(EngUnit):
+    """Creates a pressure object that can store a pressure value and 
+    convert between units of pressure."""
+    
+    class Unit():
+        bar = 'bar' 
+        mbar = 'mbar' 
+        ubar = 'ubar' 
+        Pa = 'Pa' 
+        hPa = 'hPa' 
+        kPa = 'kPa' 
+        MPa = 'MPa' 
+        kgcm2 = 'kgcm2' 
+        atm = 'atm' 
+        mmHg = 'mmHg' 
+        mmH2O = 'mmH2O' 
+        mH2O = 'mH2O' 
+        psi = 'psi' 
+        ftH2O = 'ftH2O' 
+        inH2O = 'inH2O' 
+        inHg = 'inHg' 
+    
     conversions = {
         'bar' : 1.0,
         'mbar' : 1000.0,
@@ -204,18 +288,23 @@ class PressureUnit(Unit):
         'inHg' : 29.53
     }
 
-class MassUnit(Unit):
-    g = 'g'
-    mg = 'mg'
-    kg = 'kg'
-    metricTon = 'metricTon'
-    
-    lb = 'lb'
-    slug = 'slug'
-    oz = 'oz'
-    gr = 'gr'
-    ton = 'ton'
 
+class Mass(EngUnit):
+    """Creates a mass object that can store a mass value and 
+    convert between units of mass."""
+    
+    class Unit():
+        kg = 'kg'
+        g = 'g'
+        mg = 'mg'
+        metricTon = 'metricTon'
+        lb = 'lb'
+        oz = 'oz'
+        grain = 'grain'
+        shortTon = 'shortTon'
+        longTon = 'longTon'
+        slug = 'slug'
+    
     conversions = {
         'kg' : 1.0,
         'g' : 1000.0,
@@ -223,6 +312,49 @@ class MassUnit(Unit):
         'metricTon' : 1.0 / 1000.0,
         'lb' : 2.2046226218,
         'oz' : 35.274,
-        'gr' : 2.2046226218 * 7000.0,
-        'ton' : 2.2046226218 / 2000.0
+        'grain' : 2.2046226218 * 7000.0,
+        'shortTon' : 1.0 / 907.185,
+        'longTon' : 1.0 / 1016.047,
+        'slug' : 1.0 / 14.5939029
     }
+
+
+class Force(EngUnit):
+    """Creates a foce object that can store a foce value and 
+    convert between units of foce."""
+
+    class Unit():
+        N = 'N'
+        kN = 'kN'
+        MN = 'MN'
+        GN = 'GN'
+        gf = 'gf'
+        kgf = 'kgf'
+        dyn = 'dyn'
+        Jm = 'J/m'
+        Jcm = 'J/cm'
+        shortTonF = 'shortTonF'
+        longTonF = 'longTonF'
+        kipf = 'kipf'
+        lbf = 'lbf'
+        ozf = 'ozf'
+        pdl = 'pdl'
+
+    conversions = {
+        'N' : 1.0,
+        'kN' : 1.0 / 1000.0,
+        'MN' : 1.0 / 1000000.0,
+        'GN' : 1.0 / 1000000000.0,
+        'gf' : 1.019716213e+2,
+        'kgf' : 1.019716213e-1,
+        'dyn' : 1e+5,
+        'J/m' : 1.0,
+        'J/cm' : 100.0,
+        'shortTonF' : 1.124045e-4,
+        'longTonF' : 1.003611e-4,
+        'kipf' : 2.248089e-4,
+        'lbf' : 2.248089431e-1,
+        'ozf' : 3.5969430896,
+        'pdf' : 7.2330138512
+    }
+
